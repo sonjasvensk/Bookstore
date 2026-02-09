e0ec34cfd38c9514339d4ce5526b5deff661a57a
sonjasvensk <162495225+sonjasvensk@users.noreply.github.com>
2026-02-09 10:43:04 +0200

    listpage

--- src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -1,16 +1,28 @@
 package fi.haagahelia.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import fi.haagahelia.bookstore.domain.Book;
+import fi.haagahelia.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
     @GetMapping("/index")
     public String index(Model model) {
         model.addAttribute("book", new Book());
-        return "index"; // index.html
+        return "index";
+    }
+
+    @GetMapping("/booklist")
+    public String bookList(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "booklist";
     }
 }
